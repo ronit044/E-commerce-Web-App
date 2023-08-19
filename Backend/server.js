d0af2537b71ae3c4c109c29e6f110ab1e5c5340c
ronit <ronitsharma3870@gmail.com>
2023-08-19 19:08:20 +0530

    small changes in navbar

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app=Express();
 const bodyParser=require("body-parser");
 const {MongoClient}=require("mongodb");
 const uri="mongodb+srv://ronitsharma3870:ashu6608@cluster0.rtstlsp.mongodb.net/?retryWrites=true&w=majority";
+const client=MongoClient(uri);
 const Cors=require("cors");
 app.use(Cors({origin:"http://localhost:3000",credentials:true}));
 app.use(bodyParser.urlencoded({extended:true}));
--- src/frontend/Navbar/Navbar.jsx
+++ src/frontend/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Navbar.css'; // Import the CSS file
-
+import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Login/Signup</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Cart</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Contact</a></li>
+        <Link to="/"><li className="nav-item">Home</li></Link>
+        <Link to="/Login"><li className="nav-item">Login/Signup</li></Link>
+        <Link to="/Cart"><li className="nav-item">Cart</li></Link>
+        <Link to="/Contact"><li className="nav-item">Contact</li></Link>
       </ul>
     </nav>
   );
