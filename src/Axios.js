43dff5ac539c7f3e0bb8ede739394ef556c9d8c7
ronit <ronitsharma3870@gmail.com>
2023-09-23 19:22:17 +0530

    some changes

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios=axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL:"https://ecommerce-demo-bakend.onrender.com",
     headers:{
         'Content-Type':'application/json',
     }
